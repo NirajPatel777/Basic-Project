748bf55d8c5dc2eb0ea99d42866d94dc75196b5e
NirajPatel777 <7622nirajpatel@gmail.com>
2024-01-19 12:02:02 +0530

    simple

--- src/index.ts
+++ src/index.ts
@@ -86,19 +86,3 @@ if (darkMode) {
         }
     });
 }
-
-//* in javascript menubtn and closebtn
-
-// menuBtn.addEventListener("click", () => {
-//     sideMenu.style.display = "block";
-// });
-
-// closeBtn.addEventListener('click', () => {
-//     sideMenu.style.display = 'none';
-// });
-
-// darkMode.addEventListener('click', () => {
-//     document.body.classList.toggle('dark-mode-variable');
-//     darkMode.querySelector(span:nth-child(1))?.classList.toggle('active');
-//     darkMode.querySelector(span:nth-child(2))?.classList.toggle('active');
-// })
